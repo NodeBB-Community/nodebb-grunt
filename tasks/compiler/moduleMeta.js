da3a1c0ed5b40f2572c3ed7100d67abeeb7dd97e
frissdiegurke <frissdiegurke@hotmail.com>
2015-11-03 22:51:37 +0100

    Add meta-replace for paths

--- tasks/compiler/moduleMeta.js
+++ tasks/compiler/moduleMeta.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var _ = require("lodash");
+var fs = require("fs");
 var path = require("path");
 
 module.exports.process = function (module, options, helpers) {
@@ -14,11 +15,22 @@ module.exports.process = function (module, options, helpers) {
   }
   var metaReplace = helpers.getReplacer(new RegExp(options.regex, "g"), content);
   var cwd = module.paths.tmp;
+  // replace meta within files
   _.each(grunt.file.expand({cwd: cwd}, options.src), function (filePath) {
     filePath = path.join(cwd, filePath);
     if (grunt.file.isFile(filePath)) {
       grunt.file.write(filePath, metaReplace(grunt.file.read(filePath)));
     }
   });
-  // TODO replace occurrences within file-names
+  // move files if path contains meta
+  var replaced = [];
+  _.each(grunt.file.expand({cwd: cwd}, options.src), function (filePath) {
+    filePath = path.join(cwd, filePath);
+    _.each(replaced, function (r) { filePath = filePath.replace(r.origin, r.result); });
+    var newFilePath = metaReplace(filePath);
+    if (newFilePath !== filePath) {
+      fs.renameSync(filePath, newFilePath);
+      replaced.push({origin: filePath, result: newFilePath});
+    }
+  });
 };
